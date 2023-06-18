877bb63b231f4a7e98ed32b8c157e2a336555d72
Zahar <52533016+legendary130@users.noreply.github.com>
2023-06-18 13:25:18 +0400

    Add files via upload

---
+++ Технологии_разраб/ЛБ-3(1).xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Grid1.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Grid1.Background = new SolidColorBrush(Colors.Blue);
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Grid1.Background = new SolidColorBrush(Colors.Black);
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Мой создатель Захар Быстрянцев");
+        }
+
+        private void MenuItem_MouseMove(object sender, MouseEventArgs e)
+        {
+            TextBlock1.Text = "Эта кнопка перекрашивает фон в красный";
+        }
+
+        private void MenuItem_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            TextBlock1.Text = "Эта кнопка перекрашивает фон в синий";
+        }
+
+        private void MenuItem_MouseMove_2(object sender, MouseEventArgs e)
+        {
+            TextBlock1.Text = "Эта кнопка перекрашивает фон в чёрный";
+        }
+
+        private void MenuItem_MouseMove_3(object sender, MouseEventArgs e)
+        {
+            TextBlock1.Text = "Эта кнопка выводит ФИ разработчика";
+        }
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Технологии_разраб/ЛБ-3(2).xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.Strokes.Clear();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Kist.Color = Color.FromRgb(252, 3, 3);
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Kist.Color = Color.FromRgb(74, 3, 252);
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            Kist.Color = Color.FromRgb(22, 22, 23);
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            Kist.Height += 5;
+            Kist.Width += 1;
+        }
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+            Kist.Height -= 5;
+            Kist.Width -= 1;
+        }
+    }
+}
